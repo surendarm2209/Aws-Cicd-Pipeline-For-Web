2ac45dee108f4c7394aee053932fed4e1946ee5a
surendarm2209 <surendarm2209@gmail.com>
2025-03-13 15:12:09 +0530

    Update checkout.spec.js

--- tests/specs/checkout.spec.js
+++ tests/specs/checkout.spec.js
@@ -1,38 +1,38 @@
-// import { expect } from 'chai';
+// // import { expect } from 'chai';
 
-// describe('Checkout Process', () => {
-//     it('should add an item to cart and proceed to checkout', async () => {
-//         await browser.url('/');
-//         await $('#user-name').setValue('standard_user');
-//         await $('#password').setValue('secret_sauce');
-//         await $('#login-button').click();
+// // describe('Checkout Process', () => {
+// //     it('should add an item to cart and proceed to checkout', async () => {
+// //         await browser.url('/');
+// //         await $('#user-name').setValue('standard_user');
+// //         await $('#password').setValue('secret_sauce');
+// //         await $('#login-button').click();
 
-//         await $('.inventory_item:nth-child(1) .btn_inventory').click();
-//         await $('.shopping_cart_link').click();
-//         await $('#checkout').click();
+// //         await $('.inventory_item:nth-child(1) .btn_inventory').click();
+// //         await $('.shopping_cart_link').click();
+// //         await $('#checkout').click();
 
-//         await $('#first-name').setValue('John');
-//         await $('#last-name').setValue('Doe');
-//         await $('#postal-code').setValue('12345');
-//         await $('#continue').click();
+// //         await $('#first-name').setValue('John');
+// //         await $('#last-name').setValue('Doe');
+// //         await $('#postal-code').setValue('12345');
+// //         await $('#continue').click();
         
-//         const totalPrice = await $('.summary_total_label');
-//         expect(await totalPrice.isDisplayed()).to.be.true;
+// //         const totalPrice = await $('.summary_total_label');
+// //         expect(await totalPrice.isDisplayed()).to.be.true;
 
-//         await browser.saveScreenshot('./screenshots/checkout_success.png');
-//     });
+// //         await browser.saveScreenshot('./screenshots/checkout_success.png');
+// //     });
+// // });
+
+
+
+
+// const { expect } = require('chai');
+
+// describe('Search Test', () => {
+//   it('should display search results', async () => {
+//     await browser.url('/');
+//     await $('#search-box').setValue('WebdriverIO');
+//     await $('button[type="submit"]').click();
+//     await expect($('.search-results')).toBeDisplayed();
+//   });
 // });
-
-
-
-
-const { expect } = require('chai');
-
-describe('Search Test', () => {
-  it('should display search results', async () => {
-    await browser.url('/');
-    await $('#search-box').setValue('WebdriverIO');
-    await $('button[type="submit"]').click();
-    await expect($('.search-results')).toBeDisplayed();
-  });
-});
